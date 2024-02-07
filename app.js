5c6c129cb3e424c924c0a706450ace8b44ad4cd8
Luke Sanger <lukesngr@gmail.com>
2024-02-07 21:11:24 +1100

    Backend should be working now

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var schedulesRouter = require('./routes/getSchedules');
+var createGoalRouter = require('./routes/createGoal');
+var createScheduleRouter = require('./routes/createSchedule');
+var createTimeboxRouter = require('./routes/createTimebox');
+var deleteGoalRouter = require('./routes/deleteGoal');
+var deleteScheduleRouter = require('./routes/deleteSchedule');
+var deleteTimeboxRouter = require('./routes/deleteTimebox');
+var getSchedulesRouter = require('./routes/getSchedules');
+var updateGoalRouter = require('./routes/updateGoal');
+var updateScheduleRouter = require('./routes/updateSchedule');
+var updateTimeboxRouter = require('./routes/updateTimebox');
+var createRecordedTimeboxRouter = require('./routes/createRecordedTimebox');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/getSchedules', schedulesRouter);
+app.use('/createGoal', createGoalRouter);
+app.use('/createSchedule', createScheduleRouter);
+app.use('/createTimebox', createTimeboxRouter);
+app.use('/deleteGoal', deleteGoalRouter);
+app.use('/deleteSchedule', deleteScheduleRouter);
+app.use('/deleteTimebox', deleteTimeboxRouter);
+app.use('/getSchedules', getSchedulesRouter);
+app.use('/updateGoal', updateGoalRouter);
+app.use('/updateSchedule', updateScheduleRouter);
+app.use('/updateTimebox', updateTimeboxRouter);
+app.use('/createRecordedTimebox', createRecordedTimeboxRouter);
+
+module.exports = app;
