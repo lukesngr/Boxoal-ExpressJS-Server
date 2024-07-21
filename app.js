39c8d82314b925ffe1b033ec01e4d4a4201c362e
Luke Sanger <lukesngr@gmail.com>
2024-07-21 21:04:14 +1000

    Slowly changing to UUID for improved security

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');     
 
 var indexRouter = require('./routes/index');
 var schedulesRouter = require('./routes/getSchedules');
@@ -19,21 +18,9 @@ var updateTimeboxRouter = require('./routes/updateTimeBox');
 var createRecordedTimeboxRouter = require('./routes/createRecordedTimebox');
 var oauthcallbackRouter = require('./routes/oauthcallback');
 var clearRecordingRouter = require('./routes/clearRecording');
+var createUserRouter = require('./routes/createUser');
 var app = express();
 
-const whitelist = ['http://localhost:3000', 'https://github.com', 'BoxoalApp-1233']; // assuming front-end application is running on localhost port 3000
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS '+origin))
-    }
-  }
-}
-
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -41,21 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/getSchedules', cors(corsOptions), schedulesRouter);
-app.use('/createGoal', cors(corsOptions), createGoalRouter);
-app.use('/createSchedule', cors(corsOptions), createScheduleRouter);
-app.use('/createTimebox', cors(corsOptions), createTimeboxRouter);
-app.use('/deleteGoal', cors(corsOptions), deleteGoalRouter);
-app.use('/deleteSchedule', cors(corsOptions), deleteScheduleRouter);
-app.use('/deleteTimebox', cors(corsOptions), deleteTimeboxRouter);
-app.use('/getSchedules', cors(corsOptions), getSchedulesRouter);
-app.use('/updateGoal', cors(corsOptions), updateGoalRouter);
-app.use('/updateSchedule', cors(corsOptions), updateScheduleRouter);
-app.use('/updateTimeBox', cors(corsOptions), updateTimeboxRouter);
-app.use('/createRecordedTimebox', cors(corsOptions), createRecordedTimeboxRouter);
-app.use('/oauthcallback', cors(), oauthcallbackRouter);
-app.use('/clearRecording', cors(corsOptions), clearRecordingRouter);
-/*app.use('/getSchedules', schedulesRouter);
+app.use('/getSchedules', schedulesRouter);
 app.use('/createGoal', createGoalRouter);
 app.use('/createSchedule', createScheduleRouter);
 app.use('/createTimebox', createTimeboxRouter);
@@ -65,8 +38,10 @@ app.use('/deleteTimebox', deleteTimeboxRouter);
 app.use('/getSchedules', getSchedulesRouter);
 app.use('/updateGoal', updateGoalRouter);
 app.use('/updateSchedule', updateScheduleRouter);
-app.use('/updateTimebox', updateTimeboxRouter);
+app.use('/updateTimeBox', updateTimeboxRouter);
 app.use('/createRecordedTimebox', createRecordedTimeboxRouter);
-app.use('/oauthcallback', cors(), oauthcallbackRouter);*/
+app.use('/oauthcallback', oauthcallbackRouter);
+app.use('/clearRecording', clearRecordingRouter);
+app.use('/createUser', createUserRouter);
 
 module.exports = app;
