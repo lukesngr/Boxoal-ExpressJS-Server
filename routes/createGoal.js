aa615d07b12dca4c2f8e819f775ed97450a34292
Luke Sanger <lukesngr@gmail.com>
2025-01-23 13:36:13 +1100

    big mistake not adding this error now paying for it

--- routes/createGoal.js
+++ routes/createGoal.js
@@ -11,6 +11,7 @@ router.post('/', async (req, res) => {
     });
     res.status(200).json({ message: 'Created goal successfully' });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   } finally {
     await prisma.$disconnect();
